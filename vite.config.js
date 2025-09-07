4af09b6e80bd7f6acbd158a445a1bac111cd9d6f
Your Name <you@example.com>
2025-09-07 18:21:52 -0400

    Fix: Reverter base do Vite para padrão e adicionar regras de rotas no vercel.json para assets

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mostardinha-landing/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
